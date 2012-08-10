c0b3635bd09547078d1e8db0538fcf8220c99821
joycem137 <joycem137@gmail.com>
2012-08-10 09:32:24 -0700

    Got PHP talking to SQL

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
-<?php
+<?
+$username="autotoan_user";
+$password="E2NvLU8mcAuAv8uC";
+$database="autotoan";
 
+mysql_connect(localhost,$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+
+$paragraphIndex=$_GET['paragraph'];
+$query = "SELECT * FROM paragraphs WHERE `number`=$paragraphIndex";
+$result = mysql_query($query);
+$myRow = mysql_fetch_assoc($result);
+
+mysql_close();
+
+//Get the number of rows in your array for loop 
+$numRows = mysql_numrows($result);
+
+$myParagraph = "null";
+if ($numRows > 0) {
+	$myParagraph = $myRow["data"];
+} 
+	echo "{number:$paragraphIndex,data:$myParagraph}";
 ?>
