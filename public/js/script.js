7f65b43d4c8fc538346cdd12b8be0d3158d4872a
joycem137 <joycem137@gmail.com>
2012-08-11 16:40:07 -0700

    Modified paragraph output to correctly format paragraph tables when they are provided.

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,85 @@
-function handleParagraphResponse(paragraphData) {
-    $("#paragraphResponse")[0].innerText = paragraphData;
+/**
+ * Specifically render an array of paragraph table options to HTML
+ *
+ * @param tableOptions
+ */
+function renderParagraphTable(tableOptions) {
+    var index,
+        numOptions = tableOptions.length,
+        option,
+        resultHTML = "<ol>";
+    for (index = 0; index < numOptions; index++) {
+        option = tableOptions[index];
+        resultHTML += "<li>" + option.name + " (" + option.table + ")" + "</li>";
+    }
+
+    resultHTML += "</ol>";
+    return resultHTML;
 }
 
-function processInputRequest() {
-    var paragraphNumber, paragraphObject;
-    paragraphObject = $("#paragraphNumber")[0];
-    paragraphNumber  = paragraphObject.value;
-    $.getJSON("tales.php?paragraph=" + paragraphNumber,handleParagraphResponse);
+/**
+ * Given the nature of the paragraph render it to HTML.
+ * @param paragraphData
+ */
+function renderParagraph(paragraphData) {
+
+    if (paragraphData.type === "table") {
+        // We have a table type of paragraph.
+        return renderParagraphTable(paragraphData.options);
+    }
+}
+
+/**
+ * Display a given paragraph result on the screen.
+ *
+ * @param header
+ * @param body
+ * @param showInput
+ */
+function showResult(header, body, showInput) {
+    var
+        resultPage = $("#resultPage")[0],
+        resultHeaderEl = $("#resultHeader")[0],
+        resultBodyEl = $("#resultBody")[0],
+        inputEl = $("#paragraphInput")[0];
+
+    resultHeaderEl.innerHTML = header;
+    resultBodyEl.innerHTML = body;
+
+    resultPage.style.display = "block";
+    resultHeaderEl.style.display = header ? "block" : "none";
+    resultBodyEl.style.display = body ? "block" : "none";
+    inputEl.style.display = showInput ? "block" : "none";
+}
+
+/**
+ * Process the response for a given paragraph.
+ *
+ * @param paragraph
+ */
+function handleParagraphResponse(paragraph) {
+    var headerText,
+        bodyText;
+    if (paragraph.data) {
+        headerText = "Paragraph " + paragraph.number;
+        bodyText = renderParagraph(paragraph.data);
+        showResult(headerText, bodyText, false);
+    } else {
+        headerText = "Paragraph " + paragraph.number + " not found.  You may enter a paragraph below."
+        showResult(headerText, null, true);
+    }
+}
+
+function lookupParagraph() {
+    var paragraphNumber;
+    paragraphNumber = $("#paragraphNumber")[0].value;
+    $.getJSON("tales.php?paragraph=" + paragraphNumber, handleParagraphResponse);
 }
 
 function attachListeners() {
     $('#formParagraph').keydown(function() {
         if (event.keyCode == 13) {
-            processInputRequest();
+            lookupParagraph();
             return false;
         }
     });
