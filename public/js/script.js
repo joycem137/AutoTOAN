51598e9b3634d095f485f616de8bd8c57e073518
joycem137 <joycem137@gmail.com>
2012-08-12 12:50:12 -0700

    Starting to refactor code into a more OO style.

--- public/js/script.js
+++ public/js/script.js
@@ -1,91 +1,120 @@
-/**
- * Specifically render an array of paragraph table options to HTML
- *
- * @param tableOptions
- */
-function convertTableJsonToText(tableOptions) {
-    var index,
-        numOptions = tableOptions.length,
-        option,
-        resultText = "";
-    for (index = 0; index < numOptions; index++) {
-        option = tableOptions[index];
-        resultText += (index + 1) + ". " +  option.name + " (" + option.table + ")\n";
-    }
+util = {};
 
-    return resultText;
-}
+util.ParagraphConverters = {
 
-/**
- * Given the nature of the paragraph render it to HTML.
- * @param paragraphData
- */
-function convertParagraphJsonToText(paragraphData) {
-
-    if (paragraphData.type === "table") {
-        // We have a table type of paragraph.
-        return convertTableJsonToText(paragraphData.options);
-    }
-}
-
-/**
- * Specifically render an array of paragraph table options to HTML
- *
- * @param tableOptions
- */
-function convertTableJsonToHTML(tableOptions) {
-    var index,
-        numOptions = tableOptions.length,
-        option,
-        resultHTML = "<ol>";
-    for (index = 0; index < numOptions; index++) {
-        option = tableOptions[index];
-        resultHTML += "<li>" + option.name + " (" + option.table + ")" + "</li>";
-    }
-
-    resultHTML += "</ol>";
-    return resultHTML;
-}
-
-/**
- * Given the nature of the paragraph render it to HTML.
- * @param paragraphData
- */
-function convertParagraphJsonToHTML(paragraphData) {
-
-    if (paragraphData.type === "table") {
-        // We have a table type of paragraph.
-        return convertTableJsonToHTML(paragraphData.options);
-    }
-}
-
-/**
- * Convert the provided paragraph text to JSON, or return null if invalid.
- *
- * @param paragraphText
- *
- * Add this  to enable different types of lists to be entered.
- */
-function convertParagraphTextToJSON(paragraphText) {
-    var tablePattern = /\d*(\.\s|\s|\)\s)(.*)\s\((\w)\)(\n|$)/g,
-        options, matchObj;
-
-    matchObj = tablePattern.exec(paragraphText);
-    if (matchObj) {
-        // Okay, this looks like a table.  Treat it as such.
-        options = [];
-        while(matchObj) {
-            options.push( {
-                name: matchObj[2],
-                table: matchObj[3]
-            });
-            matchObj = tablePattern.exec(paragraphText);
+    /**
+     * Provides a list of converter functions for individual types
+     * for simple lookup.
+     */
+    _converters: {
+        table:{
+            html:"_convertTableJsonToHTML",
+            text:"_convertTableJsonToText"
         }
-        return { type: "table", options: options };
-    } else {
+    },
+
+    /**
+     * Converts the provided source into the type of data specified at the target.
+     * @param source
+     * @param target
+     */
+    convert:function (source, target) {
+        var converter,
+            converterFuncName,
+            converterFunc;
+        if (typeof source === "string") {
+            // We are converting text into Json
+            return this._convertTextToJson(source);
+        } else {
+            // We are converting json to plain text or HTML
+            converter = this._converters[source.type];
+            converterFuncName = converter && converter[target];
+            converterFunc = converterFuncName && this[converterFuncName];
+            if (converterFunc) {
+                return converterFunc(source);
+            }
+        }
         return null;
+    },
+
+    /**
+     * Convert the provided paragraph text to JSON, or return null if invalid.
+     *
+     * @param paragraphText
+     *
+     * Add this  to enable different types of lists to be entered.
+     */
+    _convertTextToJson:function (paragraphText) {
+        var tablePattern = /\d*(\.\s|\s|\)\s)(.*)\s\((\w)\)(\n|$)/g,
+            options, matchObj;
+
+        matchObj = tablePattern.exec(paragraphText);
+        if (matchObj) {
+            // Okay, this looks like a table.  Treat it as such.
+            options = [];
+            while (matchObj) {
+                options.push({
+                    name:matchObj[2],
+                    table:matchObj[3]
+                });
+                matchObj = tablePattern.exec(paragraphText);
+            }
+            return { type:"table", options:options };
+        } else {
+            return null;
+        }
+    },
+
+    /**
+     * Specifically render an array of paragraph table options to HTML
+     *
+     * @param tableOptions
+     */
+    _convertTableJsonToText:function (paragraphData) {
+        var tableOptions = paragraphData.options,
+            index,
+            numOptions = tableOptions.length,
+            option,
+            resultText = "";
+        for (index = 0; index < numOptions; index++) {
+            option = tableOptions[index];
+            resultText += (index + 1) + ". " + option.name + " (" + option.table + ")\n";
+        }
+
+        return resultText;
+    },
+
+    /**
+     * Specifically render an array of paragraph table options to HTML
+     *
+     * @param tableOptions
+     */
+    _convertTableJsonToHTML:function (paragraphData) {
+        var tableOptions = paragraphData.options,
+            index,
+            numOptions = tableOptions.length,
+            option,
+            resultHTML = "<ol>";
+        for (index = 0; index < numOptions; index++) {
+            option = tableOptions[index];
+            resultHTML += "<li>" + option.name + " (" + option.table + ")" + "</li>";
+        }
+
+        resultHTML += "</ol>";
+        return resultHTML;
     }
-}
+};
+
+util.parseEncounterName = function(name) {
+    var pattern = /(\S*)\s(.*)/g,
+        matchObj;
+
+    matchObj = pattern.exec(name);
+    return {
+        adjective:matchObj[1],
+        noun:matchObj[2]
+    };
+};
 
 /**
  * Display a given paragraph result on the screen.
@@ -121,7 +150,7 @@ function switchToResultEditMode() {
         textAreaEl = $("#paragraphInputFromUser")[0]
         ;
 
-    textAreaEl.value = convertParagraphJsonToText(cachedData);
+    textAreaEl.value = util.ParagraphConverters.convert(cachedData, "text");
 
     resultBodyEl.style.display = "none";
     inputEl.style.display = "block";
@@ -141,23 +170,12 @@ function handleParagraphResponse(paragraph) {
     if (paragraph.data) {
         cachedData = paragraph.data;
         headerText = "Paragraph " + paragraph.number;
-        bodyText = convertParagraphJsonToHTML(paragraph.data);
+        bodyText = util.ParagraphConverters.convert(paragraph.data, "html");
         showResult(headerText, bodyText, false);
     } else {
         headerText = "Paragraph " + paragraph.number + " not found.  You may enter a paragraph below."
         showResult(headerText, null, true);
     }
-}
-
-function parseEncounterName(name) {
-    var pattern = /(\S*)\s(.*)/g,
-        matchObj;
-
-    matchObj = pattern.exec(name);
-    return {
-        adjective:matchObj[1],
-        noun:matchObj[2]
-    };
 }
 
 function selectUserAction(actions, paragraphs) {
@@ -182,7 +200,7 @@ function selectUserAction(actions, paragraphs) {
 function displayEncounterOptions(tableId, encounterName) {
     var table = reactionTables[tableId.toUpperCase()],
         actions = table.actions,
-        adjective = parseEncounterName(encounterName).adjective.toLowerCase(),
+        adjective = util.parseEncounterName(encounterName).adjective.toLowerCase(),
         paragraphs = table.adjectives[adjective];
 
     selectUserAction(actions, paragraphs);
@@ -212,7 +230,7 @@ function updateParagraph() {
         updatedParagraph = $('#paragraphInputFromUser')[0].value,
         paragraphData;
 
-    paragraphData = convertParagraphTextToJSON(updatedParagraph);
+    paragraphData = util.ParagraphConverters.convert(updatedParagraph);
 
     if (paragraphData) {
         $.post("tales.php", { paragraph:paragraphNumber, updateData:paragraphData }, handleParagraphResponse);
